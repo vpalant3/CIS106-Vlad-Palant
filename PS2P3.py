3c41ca726f733382b9df0d41a769bb44dac26e9e
vpalant3 <158368851+vpalant3@users.noreply.github.com>
2024-02-09 17:44:47 -0600

    Delete PS2P3.py

--- PS2P3.py
+++
@@ -1,3 +0,0 @@
-total_amount_received = float(input("Enter the total amount received for the job: "))
-amount_per_person = total_amount_received / 3
-print(f"Each person will receive: ${amount_per_person:.2f}")
