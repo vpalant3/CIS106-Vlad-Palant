3810ce1eb7a9c96921c8fa04ebeab3eab6f6dac3
vpalant3 <158368851+vpalant3@users.noreply.github.com>
2024-02-09 17:45:14 -0600

    Delete PS2P1.py

--- PS2P1.py
+++
@@ -1,6 +0,0 @@
-stock_ticker_symbol = input("Enter the stock ticker symbol: ")
-number_of_shares = int(input("Enter the number of shares: "))
-cost_per_share = float(input("Enter the cost per share: "))
-
-amount_invested = number_of_shares * cost_per_share
-print("Amount invested:", amount_invested)
